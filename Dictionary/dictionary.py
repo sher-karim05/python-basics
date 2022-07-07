98090850e8085a55e49369e53609b678ae89d5ff
sher-karim05 <karimdeveloper12@gmail.com>
2022-07-08 03:32:33 +0500

    python dictionary created

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,16 @@
+# Dictionary is a collection of key value pairs, items can be changed but can not contain dulicate key
+monthConversioins = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+print(monthConversioins.get("lil", "Not a valid key"))
