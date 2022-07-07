9d113f358d40e40c6751732f263741f61d8a1543
sher-karim05 <karimdeveloper12@gmail.com>
2022-07-08 03:33:01 +0500

    python dictionary created

--- Dictionaries/Dictionaries.py
+++
@@ -1,4 +0,0 @@
- # Dictionary is a collecion which is ordered, changeable, and dublicate keys are not allowed.
- # Dictionaries are used to store data values in key: value pairs such as  cat = { "name": "Tomy"}
-
-
